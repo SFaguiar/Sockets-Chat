38c628ef1dd9c207991d74c7db2f2154c4196814
Samuel Aguiar <samuelfaguiar107@gmail.com>
2023-12-12 15:48:32 -0300

    venv + troca de chaves

--- client.py
+++ client.py
@@ -6,7 +6,11 @@ Início: 25/11/2023
 
 import socket
 import threading
+import rsa
 
+PUBLIC_KEY, PRIVATE_KEY = rsa.newkeys(1024)
+
+# Configurações do cliente
 CHAR_SET = 'utf-8'
 IP_SERVIDOR = '127.0.0.1'
 PORTA_SERVIDOR = 1945
@@ -18,15 +22,20 @@ nickname = input("[CLIENTE] Escolha seu apelido: ")
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((IP_SERVIDOR, PORTA_SERVIDOR))
 
-
 # Função para receber mensagens do servidor
 def receive() -> None:
     while True:
         try:
             # Se receber a palavra-chave 'NICK', enviar o apelido selecionado
             message = client.recv(1024).decode(CHAR_SET)
-            if message == 'NICK':
+            if message == 'CLIENT_NICKNAME':
+                # Enviar o apelido selecionado ao servidor
                 client.send(nickname.encode(CHAR_SET))
+            elif message == 'CLIENT_PUBLIC_KEY':
+                # Enviar a chave pública ao servidor
+                client.send(PUBLIC_KEY.save_pkcs1("PEM"))
+            elif message == 'SERVER_PUBLIC_KEY':
+                SERVER_PUBLIC_KEY = rsa.PublicKey.load_pkcs1(client.recv(1024))
             else:
                 # Imprimir a mensagem recebida, caso o nickname já tenha sido enviado
                 print(message)
--- server.py
+++ server.py
@@ -6,12 +6,16 @@ Início: 25/11/2023
 
 import socket
 import threading
+import rsa
+
 
 class User:
-    def __init__(self, nickname: str, client: socket.socket) -> None:
+    def __init__(self, nickname:str, client:socket.socket, key:bytes) -> None:
         self.client = client
         self.nickname = nickname
-        
+
+# Gerar chaves pública e privada para o servidor
+PUBLIC_KEY, PRIVATE_KEY = rsa.newkeys(1024)
 
 # Constantes para a criação do servidor
 PORT = 1945
@@ -32,8 +36,9 @@ def broadcast(message:bytes) -> None:
     for user in users:
         user.client.send(message)
 
+
 # Função para tratar os clientes
-def handle(user: User) -> None:
+def handle(user:User) -> None:
     while True:
         try:
             # Enviar mensagens para todos os clientes se houverem mensagens para enviar
@@ -57,14 +62,24 @@ def receive() -> None:
         print(f"[SERVER] Conexão de {str(address)}.")
 
         # Enviar mensagem de boas vindas e pedir o nickname do cliente
-        client.send('NICK'.encode(CHAR_SET))
+        # RECEBER NICKNAME DO CLIENTE
+        client.send('CLIENT_NICKNAME'.encode(CHAR_SET))
         nickname = client.recv(1024).decode(CHAR_SET)
-        user = User(nickname, client)
+        
+        # RECEBER CHAVE PÚBLICA DO CLIENTE
+        client.send('CLIENT_PUBLIC_KEY'.encode(CHAR_SET))
+        key = rsa.PublicKey.load_pkcs1(client.recv(1024))
+        
+        # ENVIAR CHAVE PÚBLICA DO SERVIDOR PARA O CLIENTE
+        client.send('SERVER_PUBLIC_KEY'.encode(CHAR_SET))
+        client.send(PUBLIC_KEY.save_pkcs1("PEM"))
+        
+        user = User(nickname, client, key)
         users.append(user)
 
         # Imprimir o nickname do cliente e notificar conexão
         print(f"[SERVER] O apelido de {address} é {nickname}.")
-        broadcast(f"[SERVIDOR] {nickname} se juntou ao chat!".encode())
+        broadcast(f"[SERVIDOR] {nickname} se juntou ao chat!".encode(CHAR_SET))
 
         # Dar boas-vindas (TO DO: ferramenta para mudar as boas vindas).
         client.send('[SERVIDOR] Você se conectou ao servidor!'.encode(CHAR_SET))
