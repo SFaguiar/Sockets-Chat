7081ce7abb1df85c6d2a841a90748599dfa4e3fc
Samuel Aguiar <samuelfaguiar107@gmail.com>
2023-12-13 17:48:29 -0300

    Add. criptografia usuário-servidor-usuário
    Adicionada criptografia entre o usuário e o servidor. Ainda não há criptografia ponta-a-ponta de fato, pois o servidor consegue ler os dados do cliente.

--- client.py
+++ client.py
@@ -7,11 +7,14 @@ Início: 25/11/2023
 import socket
 import threading
 import rsa
+import sys
+import time
 
-PUBLIC_KEY, PRIVATE_KEY = rsa.newkeys(1024)
+# Variáveis para criptografia
+CLIENT_PUBLIC_KEY, CLIENT_PRIVATE_KEY = rsa.newkeys(1024)
 
 # Configurações do cliente
-CHAR_SET = 'utf-8'
+CHAR_SET = 'utf8'
 IP_SERVIDOR = '127.0.0.1'
 PORTA_SERVIDOR = 1945
 
@@ -22,35 +25,48 @@ nickname = input("[CLIENTE] Escolha seu apelido: ")
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((IP_SERVIDOR, PORTA_SERVIDOR))
 
+# Troca de informações com o servidor
+while True:
+    command = client.recv(1024).decode(CHAR_SET)
+    if command == 'CLIENT_NICKNAME':
+        # Enviar o apelido selecionado ao servidor
+        client.send(nickname.encode(CHAR_SET))
+    elif command == 'KEYS':
+        client.send(CLIENT_PUBLIC_KEY.save_pkcs1('PEM'))
+        SERVER_PUBLIC_KEY = rsa.PublicKey.load_pkcs1((client.recv(1024)))
+        break
+    else:
+        print(f"[CLIENTE] Ocorreu um erro! Fechando o cliente.")
+        client.close()
+        break
+
+
 # Função para receber mensagens do servidor
 def receive() -> None:
     while True:
         try:
-            # Se receber a palavra-chave 'NICK', enviar o apelido selecionado
-            message = client.recv(1024).decode(CHAR_SET)
-            if message == 'CLIENT_NICKNAME':
-                # Enviar o apelido selecionado ao servidor
-                client.send(nickname.encode(CHAR_SET))
-            elif message == 'CLIENT_PUBLIC_KEY':
-                # Enviar a chave pública ao servidor
-                client.send(PUBLIC_KEY.save_pkcs1("PEM"))
-            elif message == 'SERVER_PUBLIC_KEY':
-                SERVER_PUBLIC_KEY = rsa.PublicKey.load_pkcs1(client.recv(1024))
-            else:
-                # Imprimir a mensagem recebida, caso o nickname já tenha sido enviado
-                print(message)
-        except:
+            # Descriptografa a mensagem com a minha chave:
+            message = client.recv(1024)
+            message = rsa.decrypt(message, CLIENT_PRIVATE_KEY).decode(CHAR_SET)
+            print(message)
+        except Exception as e:
             # Fechar a conexão em caso de erro
-            print("[CLIENTE] Ocorreu um erro! Fechando o cliente.")
+            print(F"[CLIENTE] Ocorreu um erro! Fechando o cliente.")
+            print(e)
             client.close()
-            break
+            sys.exit()
 
 
 # Função para enviar mensagens para o servidor
 def write() -> None:
     while True:
-        message = input("[CLIENTE] Digite sua mensagem: ")
-        client.send(message.encode(CHAR_SET))
+        time.sleep(1)
+        message = input()
+        # Criptografa a mensagem com a chave do servidor:
+        message = rsa.encrypt(message.encode(CHAR_SET), SERVER_PUBLIC_KEY)
+
+        # Envia a mensagem criptografada ao servidor:        
+        client.send(message)
 
 
 # Iniciar threads para receber e enviar mensagens
--- server.py
+++ server.py
@@ -13,14 +13,15 @@ class User:
     def __init__(self, nickname:str, client:socket.socket, key:bytes) -> None:
         self.client = client
         self.nickname = nickname
+        self.key = key
 
 # Gerar chaves pública e privada para o servidor
-PUBLIC_KEY, PRIVATE_KEY = rsa.newkeys(1024)
+SERVER_PUBLIC_KEY, SERVER_PRIVATE_KEY = rsa.newkeys(1024)
 
 # Constantes para a criação do servidor
 PORT = 1945
 SERVER_IP = socket.gethostbyname(socket.gethostname())
-CHAR_SET = 'utf-8'
+CHAR_SET = 'utf8'
 
 # Inicialização do servidor
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -32,10 +33,18 @@ print(f"[LOG] SERVIDOR INICIADO EM {SERVER_IP}.")
 users = []
 
 # Função para enviar mensagens para todos os clientes
-def broadcast(message:bytes) -> None:
+def broadcast(message:str) -> None:
     for user in users:
+        # Enviar mensagem criptografada para todos os clientes
+        message = message.encode(CHAR_SET)
+        message = rsa.encrypt(message, user.key)
         user.client.send(message)
 
+def send_private_message(message:str, user:User) -> None:
+    message = message.encode(CHAR_SET)
+    message = rsa.encrypt(message, user.key)
+    user.client.send(message)
+    
 
 # Função para tratar os clientes
 def handle(user:User) -> None:
@@ -43,14 +52,15 @@ def handle(user:User) -> None:
         try:
             # Enviar mensagens para todos os clientes se houverem mensagens para enviar
             message = user.client.recv(1024)
-            message = f"[{user.nickname}]: {message.decode(CHAR_SET)}".encode(CHAR_SET)
+            message = rsa.decrypt(message, SERVER_PRIVATE_KEY).decode(CHAR_SET)
+            message = f"[{user.nickname}]: {message}"
             broadcast(message)
         except:
             # Remover cliente e nickname quando o cliente desconectar
             nickname = user.nickname
             users.remove(user)
             user.client.close()
-            broadcast(f'{nickname} saiu!'.encode(CHAR_SET))
+            broadcast(f'{nickname} saiu!')
             break
 
 
@@ -61,28 +71,24 @@ def receive() -> None:
         client, address = server.accept()
         print(f"[SERVER] Conexão de {str(address)}.")
 
-        # Enviar mensagem de boas vindas e pedir o nickname do cliente
-        # RECEBER NICKNAME DO CLIENTE
+        # TO DO: Enviar mensagem de boas vindas
+        # PEDIR E RECEBER NICKNAME DO CLIENTE
         client.send('CLIENT_NICKNAME'.encode(CHAR_SET))
         nickname = client.recv(1024).decode(CHAR_SET)
-        
-        # RECEBER CHAVE PÚBLICA DO CLIENTE
-        client.send('CLIENT_PUBLIC_KEY'.encode(CHAR_SET))
-        key = rsa.PublicKey.load_pkcs1(client.recv(1024))
-        
-        # ENVIAR CHAVE PÚBLICA DO SERVIDOR PARA O CLIENTE
-        client.send('SERVER_PUBLIC_KEY'.encode(CHAR_SET))
-        client.send(PUBLIC_KEY.save_pkcs1("PEM"))
-        
-        user = User(nickname, client, key)
+
+        # TROCA DE CHAVES PÚBLICAS ENTRE O CLIENTE E O SERVIDOR
+        client.send('KEYS'.encode(CHAR_SET))
+        client_key = rsa.PublicKey.load_pkcs1((client.recv(1024)))
+        client.send(SERVER_PUBLIC_KEY.save_pkcs1('PEM'))
+
+        user = User(nickname, client, client_key)
         users.append(user)
 
         # Imprimir o nickname do cliente e notificar conexão
         print(f"[SERVER] O apelido de {address} é {nickname}.")
-        broadcast(f"[SERVIDOR] {nickname} se juntou ao chat!".encode(CHAR_SET))
+        broadcast(f"[SERVIDOR] {nickname} se juntou ao chat!")
 
         # Dar boas-vindas (TO DO: ferramenta para mudar as boas vindas).
-        client.send('[SERVIDOR] Você se conectou ao servidor!'.encode(CHAR_SET))
 
         # Iniciar thread para tratar o cliente
         thread = threading.Thread(target=handle, args=(user,))
